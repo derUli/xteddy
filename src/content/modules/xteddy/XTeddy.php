2fbe3468e031d5dd2e779f1d548c61696ad78557
Ulrich Schmidt <admin@deruli.de>
2017-08-10 18:43:07 +0200

    refakturiert

--- src/content/modules/xteddy/XTeddy.php
+++ src/content/modules/xteddy/XTeddy.php
@@ -1,34 +1,41 @@
 <?php
-class XTeddy extends Controller {
-	private $moduleName = "xteddy";
-	public function getImages() {
-		$images = array ();
-		$dir = scandir ( ModuleHelper::buildModuleRessourcePath ( $this->moduleName, "images" ) );
-		foreach ( $dir as $file ) {
-			if (endsWith ( $file, ".png" )) {
-				$teddyName = basename ( pathinfo ( $file, PATHINFO_FILENAME ) );
-				$images [$teddyName] = ModuleHelper::buildModuleRessourcePath ( $this->moduleName, "images/" . basename ( $file ) );
-			}
-		}
-		return $images;
-	}
-	public function contentFilter($html) {
-		if (strpos ( $html, '[' ) !== false) {
-			$images = $this->getImages ();
-			
-			foreach ( $images as $key => $value ) {
-				ViewBag::set ( "teddy_image", $value );
-				$imgHtml = Template::executeModuleTemplate ( $this->moduleName, "teddy.php" );
-				$html = str_ireplace ( '[' . $key . ']', $imgHtml, $html );
-			}
-			if (strpos ( $html, '[xtoys]' ) !== false) {
-				$random = array_rand ( $images );
-				ViewBag::set ( "teddy_image", $images [$random] );
-				$imgHtml = Template::executeModuleTemplate ( $this->moduleName, "teddy.php" );
-				
-				$html = str_ireplace ( '[xtoys]', $imgHtml, $html );
-			}
-		}
-		return $html;
-	}
+
+class XTeddy extends Controller
+{
+
+    private $moduleName = "xteddy";
+
+    public function getImages()
+    {
+        $images = array();
+        $dir = scandir(ModuleHelper::buildModuleRessourcePath($this->moduleName, "images"));
+        foreach ($dir as $file) {
+            if (endsWith($file, ".png")) {
+                $teddyName = basename(pathinfo($file, PATHINFO_FILENAME));
+                $images[$teddyName] = ModuleHelper::buildModuleRessourcePath($this->moduleName, "images/" . basename($file));
+            }
+        }
+        return $images;
+    }
+
+    public function contentFilter($html)
+    {
+        if (strpos($html, '[') !== false) {
+            $images = $this->getImages();
+            
+            foreach ($images as $key => $value) {
+                ViewBag::set("teddy_image", $value);
+                $imgHtml = Template::executeModuleTemplate($this->moduleName, "teddy.php");
+                $html = str_ireplace('[' . $key . ']', $imgHtml, $html);
+            }
+            if (strpos($html, '[xtoys]') !== false) {
+                $random = array_rand($images);
+                ViewBag::set("teddy_image", $images[$random]);
+                $imgHtml = Template::executeModuleTemplate($this->moduleName, "teddy.php");
+                
+                $html = str_ireplace('[xtoys]', $imgHtml, $html);
+            }
+        }
+        return $html;
+    }
 }
